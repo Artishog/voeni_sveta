c723fe56836229074d0a004be26aedce5d959bcc
Oleg Kharko <kharko1993@gmail.com>
2014-03-27 16:11:52 +0200

    Test Created Application
    Added TestCreating Application

---
+++ TetsCreatorApplication/TetsCreatorApplication/Model/Task.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TetsCreatorApplication.Model
+{
+    public class Task
+    {
+        public TaskType Type { get; set; }
+        public String Question { get; set; }
+        public String Answer { get; set; }
+
+
+
+    }
+}
---
+++ TetsCreatorApplication/TetsCreatorApplication/Model/Test.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace TetsCreatorApplication.Model
+{
+    public class Test
+    {
+        public List<Task> Tasks { get; set; }
+
+        public int TaskCount
+        {
+            get
+            {
+                if (Tasks != null)
+                {
+                    return Tasks.Count;
+                }
+                return 0;
+            }
+        }
+
+        public int ReservedTime { get; set; }
+
+        public int MinBalllsForCompliteTest { get; set; }
+
+
+        #region TestFlags
+
+        public bool IsAnswerShows { get; set; }
+
+        public bool IsTimeLimited { get; set; }
+ 
+
+        #endregion
+
+
+
+    }
+}
